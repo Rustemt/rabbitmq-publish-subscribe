eaedb0958a444a7e926b991cb7e7b70f538d2596
William Lord <william.lord@7digital.com>
2013-03-20 15:39:31 +0000

    Add solution and empty publisher and subscriber projects

---
+++ Publisher/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Publisher
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+		}
+	}
+}
---
+++ Subscriber/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Subscriber
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+		}
+	}
+}
